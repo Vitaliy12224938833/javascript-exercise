60115070aeadd0c195549c0157622864ad35f19b
Vitaliy12224938833 <vitalikd247@gmail.com>
2022-11-23 02:14:57 +0300

    Head

---
+++ Solutions/Data structure/MinHeap.js
@@ -0,0 +1,103 @@
+class MinHeap {
+  constructor() {
+    this.data = [];
+    this.length = 0;
+  }
+
+  peec() {
+    return this.data[0];
+  }
+
+  size() {
+    return this.length;
+  }
+  add(value) {
+    this.data[this.length] = value;
+    this.heapifyUp(this.length);
+    this.length++;
+  }
+
+  addArray(array) {
+    for (const value of array) {
+      this.add(value);
+    }
+  }
+
+  delete() {
+    if (this.length === 0) return;
+
+    const out = this.data[0];
+    this.length--;
+    if (this.length === 0) {
+      this.data = [];
+      return out;
+    }
+
+    this.data[0] = this.data[this.length];
+    this.heapifyDown(0);
+    return out;
+  }
+
+  heapifyUp(index) {
+    if (!index) return;
+
+    const parent = Math.floor((index - 1) / 2);
+    const parentValue = this.data[parent];
+    const value = this.data[index];
+
+    if (parentValue > value) {
+      this.data[index] = parentValue;
+      this.data[parent] = value;
+      this.heapifyUp(parent);
+    }
+  }
+
+  heapifyDown(index) {
+    const leftIndex = index * 2 + 1;
+    const rightIndex = index * 2 + 2;
+
+    if (index >= this.length || leftIndex >= this.length) return;
+
+    const leftValue = this.data[leftIndex];
+    const rightValue = this.data[rightIndex];
+    const value = this.data[index];
+
+    if (leftValue >= rightValue && value >= rightValue) {
+      this.data[index] = rightValue;
+      this.data[rightIndex] = value;
+      this.heapifyDown(rightIndex);
+    } else if (rightValue >= leftValue && value >= leftValue) {
+      this.data[index] = leftValue;
+      this.data[leftIndex] = value;
+      this.heapifyDown(leftIndex);
+    }
+  }
+}
+
+const array = [
+  3554, 2227, 8866, 9890, 212, 8669, 2423, 7651, 3878, 3379, 1419, 6134, 5767,
+  859, 2848, 9309, 1449, 8408, 8041, 3367, 6676, 6382, 4136, 4871,
+];
+
+function cookies(minV, array) {
+  const heap = new MinHeap();
+  heap.addArray(array);
+  let counter = 0;
+  while (heap.size() > 1) {
+    if (heap.peec() > minV) {
+      break;
+    }
+    const fMin = heap.delete();
+    const sMin = heap.delete();
+    const sum = fMin + 2 * sMin;
+
+    heap.add(sum);
+    counter++;
+  }
+  if (heap.peec() < minV) {
+    counter = -1;
+  }
+  return counter;
+}
+
+console.log(cookies(47245, array));
---
+++ Solutions/Data structure/binary_tree.js
@@ -0,0 +1,135 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BineryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  getRoot() {
+    return this.root;
+  }
+
+  add(value) {
+    const node = new Node(value);
+
+    if (!this.root) {
+      this.root = node;
+    }
+
+    let curr = this.root;
+    while (curr) {
+      if (curr.value === value) return;
+
+      const dir = curr.value > value ? "left" : "right";
+
+      if (!curr[dir]) {
+        curr[dir] = node;
+      }
+      curr = curr[dir];
+    }
+  }
+
+  revers(node = this.root) {
+    if (!node) return;
+    [node.left, node.right] = [node.right, node.left];
+    this.revers(node.left);
+    this.revers(node.right);
+  }
+
+  addRevers(value) {
+    const node = new Node(value);
+
+    if (!this.root) {
+      this.root = node;
+    }
+
+    let curr = this.root;
+    while (curr) {
+      if (curr.value === value) return;
+
+      const dir = curr.value < value ? "left" : "right";
+
+      if (!curr[dir]) {
+        curr[dir] = node;
+      }
+      curr = curr[dir];
+    }
+  }
+
+  remove(value) {
+    if (!this.root) return null;
+
+    let target = this.root;
+    let parent = this.root;
+    if (this.root.left.value < this.root.right.value) {
+      while (target.value !== value && target) {
+        parent = target;
+        if (value > target.value) {
+          target = target.right;
+        } else target = target.left;
+      }
+    } else {
+      while (target.value !== value && target) {
+        parent = target;
+        if (value < target.value) {
+          target = target.right;
+        } else target = target.left;
+      }
+    }
+
+    const child = (target.left ? 1 : 0) + (target.right ? 1 : 0);
+    const dir = parent.left.value === target.value ? "left" : "right";
+
+    if (child === 0) {
+      parent[dir] = null;
+      return;
+    }
+
+    if (child === 1) {
+      const dirTarget = target.left ? "left" : "right";
+      parent[dir] = target[dirTarget];
+      return;
+    }
+    if (child === 2) {
+      const newChild = this.findMinValue(target.right);
+      this.remove(newChild);
+      if (parent.value === value) {
+        parent.value = newChild;
+        return;
+      }
+      parent[dir].value = newChild;
+      return;
+    }
+  }
+
+  findMaxValue(node = this.root) {
+    if (!node) return;
+    return node.right ? this.findMaxValue(node.right) : node.value;
+  }
+
+  findMinValue(node = this.root) {
+    if (!node) return;
+    return node.left ? this.findMinValue(node.left) : node.value;
+  }
+}
+
+const node = new BineryTree();
+node.add(23);
+node.add(3);
+node.add(223);
+node.add(32);
+node.add(2);
+node.add(4);
+node.add(243);
+node.add(43);
+node.add(26);
+node.revers();
+node.remove(32);
+node.remove(23);
+console.log(node.getRoot());
